24313efed1bb25f68b45fa2df539e88c99cf4da4
Andrew McCalip <andrewmccalip@gmail.com>
2025-04-10 22:36:19 -0700

    Download link

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
@@ -6,10 +6,6 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
-
-@app.route('/download')
-def download():
-    return send_from_directory('static', 'shiftkeys-setup.exe')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=False) 
