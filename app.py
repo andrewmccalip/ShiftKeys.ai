0bf5891ed4914d53daa4b3127d3228871a1583fe
Andrew McCalip <andrewmccalip@gmail.com>
2025-04-10 22:31:32 -0700

    adding installer

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/download')
+def download():
+    return send_from_directory('static', 'shiftkeys-setup.exe')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=False) 
