223ea6f0407f45a50c1c3946d0c92d1f737ad0ea
Andrew McCalip <andrewmccalip@gmail.com>
2025-04-17 19:31:59 -0700

    verison checking

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import os
 
 app = Flask(__name__)
 
+# Define the application version
+APP_VERSION = "0.20"  # You can update this as needed
+
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/latest_version')
+def latest_version():
+    return jsonify({"version": APP_VERSION})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=False) 
